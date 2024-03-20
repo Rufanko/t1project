307c04f7e9c32f6e6ae240592df431ba6d6f25b6
Rufanko <Rufanko@mail.ru>
2024-03-21 00:33:48 +0300

    Add 2 data classes

---
+++ api-tests/src/main/java/dateClasses/Token.java
@@ -0,0 +1,14 @@
+package dateClasses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Token {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+}
---
+++ api-tests/src/main/java/dateClasses/User.java
@@ -0,0 +1,13 @@
+package dateClasses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String userName;
+    private String password;
+
+}
