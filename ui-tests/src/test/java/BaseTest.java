f16decaedf7bc59387985cde3836b635a53982ff
Rufanko <Rufanko@mail.ru>
2024-04-04 13:52:28 +0300

    refactor

--- ui-tests/src/test/java/BaseTest.java
+++ ui-tests/src/test/java/BaseTest.java
@@ -1,10 +1,8 @@
-
-import com.codeborne.selenide.*;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.closeWindow;
@@ -13,8 +11,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class BaseTest {
     @BeforeEach
     public void option () {
-
-
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\.m2\\repository\\webdriver\\chromedriver\\win32\\114.0.5735.90\\chromedriver.exe");
         Configuration.baseUrl = ("https://the-internet.herokuapp.com/");
         Configuration.timeout = 15000;
